18053266ea03f8fbea4672a611ef0f1af7592e8c
灰色飘零 <1418757529@qq.com>
2019-02-21 11:21:13 +0800

    抽取音视频代码提交

--- FFmpeg/practice/repos/FFmpegTest/FFmpegTest/FFmpegTest.cpp
+++ FFmpeg/practice/repos/FFmpegTest/FFmpegTest/FFmpegTest.cpp
@@ -137,8 +137,9 @@ int h264_extradata_to_annexb(const uint8_t *codec_extradata, const int codec_ext
 }
 
 // For extractVideo
-int h264_mp4toannexb(AVFormatContext *fmt_ctx, AVPacket *in, FILE *dst_fd)
-{
+
+// 参数1：AVFormatContext上下文；参数2：读取的编码状态下的帧；参数3：目标文件
+int h264_mp4toannexb(AVFormatContext *fmt_ctx, AVPacket *in, FILE *dst_fd) {
 
 	AVPacket *out = NULL;
 	AVPacket spspps_pkt;
@@ -208,13 +209,9 @@ int h264_mp4toannexb(AVFormatContext *fmt_ctx, AVPacket *in, FILE *dst_fd)
 		if (/*s->new_idr && */unit_type == 5 /*&& !s->idr_sps_seen && !s->idr_pps_seen*/) {
 
 			h264_extradata_to_annexb(fmt_ctx->streams[in->stream_index]->codec->extradata,
-				fmt_ctx->streams[in->stream_index]->codec->extradata_size,
-				&spspps_pkt,
-				AV_INPUT_BUFFER_PADDING_SIZE);
+				fmt_ctx->streams[in->stream_index]->codec->extradata_size, &spspps_pkt, AV_INPUT_BUFFER_PADDING_SIZE);
 
-			if ((ret = alloc_and_copy(out,
-				spspps_pkt.data, spspps_pkt.size,
-				buf, nal_size)) < 0)
+			if ((ret = alloc_and_copy(out, spspps_pkt.data, spspps_pkt.size, buf, nal_size)) < 0)
 				goto fail;
 			/*s->new_idr = 0;*/
 		/* if only SPS has been seen, also insert PPS */
@@ -239,7 +236,6 @@ int h264_mp4toannexb(AVFormatContext *fmt_ctx, AVPacket *in, FILE *dst_fd)
 			}
 			*/
 		}
-
 
 		len = fwrite(out->data, 1, out->size, dst_fd);
 		if (len != out->size) {
@@ -364,15 +360,12 @@ void ffmpegVideoMeta() {
 }
 
 // 使用FFmpeg从视频中抽取音频
-void extractAudio() {
-
-	/******************************************************
-	 * TODO：目前存在抽取后不能播放的问题
-	 * 推测1：Header 不对，建议研究AAC后尝试调试
-	 * 推测2：转码的MP4的ACC格式不对，也需要研究ACC
-	 * 感觉代码写的没问题，此段代码非常需要调好，但是需要学习
-	 * 的知识也很多，慢慢来吧。
-	 ******************************************************/
+void extractAudio_HE_ACC() {
+	/************************************************************
+	 * 目前只能抽取HE-AAC格式的MP4文件(LC及其他格式抽取后不能播放)。
+	 * 
+	 * TODO：研究HE-AAC和LC-AAC的区别，整理一下AAC的格式相关的知识
+	 ************************************************************/
 
 	// 设置日志输出等级
 	av_log_set_level(AV_LOG_INFO); 
@@ -440,6 +433,7 @@ void extractAudio() {
 			if (len != pkt.size) {
 				av_log(NULL, AV_LOG_WARNING, "Waring! Length of data not equal size of pkt!\n");
 			}
+			fflush(dst_fd);
 		}
 		// 将引用基数减一
 		av_packet_unref(&pkt);
@@ -451,6 +445,129 @@ void extractAudio() {
 	if (dst_fd) {
 		fclose(dst_fd);
 	}
+}
+void extractAudio() {
+	/**
+	* 调用 av_guess_format 让ffmpeg帮你找到一个合适的文件格式。
+	* 调用 avformat_new_stream 为输出文件创建一个新流。
+	* 调用 avio_open 打开新创建的文件。
+	* 调用 avformat_write_header 写文件头。
+	* 调用 av_interleaved_write_frame 写文件内容。
+	* 调用 av_write_trailer 写文件尾。
+	* 调用 avio_close 关闭文件。
+	**/
+
+	av_log_set_level(AV_LOG_INFO);
+	av_register_all();
+
+	FILE *dst_fd = NULL;
+
+	int ret;
+
+	AVFormatContext *fmt_ctx = NULL;  // 输入文件的AVFormatContext上下文
+	AVStream *in_stream = NULL;
+
+	AVPacket pkt;
+
+	AVFormatContext *ofmt_ctx = NULL;
+	AVOutputFormat *output_fmt = NULL; 
+	AVStream *out_stream = NULL;
+	
+	int audio_stream_index = -1;
+
+	ret = avformat_open_input(&fmt_ctx, "1111.mp4", NULL, NULL);
+
+	// 检查打开输入文件是否成功
+	if (ret < 0) {
+		printf("cant open file，error message = %s", av_err2str(ret));
+		return;
+	}
+
+	/*retrieve audio stream*/
+	if ((ret = avformat_find_stream_info(fmt_ctx, NULL)) < 0) {
+		av_log(NULL, AV_LOG_DEBUG, "failed to find stream information!\n");
+		return;
+	}
+
+	in_stream = fmt_ctx->streams[1];
+	
+	AVCodecParameters *in_codecpar = in_stream->codecpar;
+	if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO) {
+		av_log(NULL, AV_LOG_ERROR, "The Codec type is invalid!\n");
+		return;
+	}
+
+	//out file
+	ofmt_ctx = avformat_alloc_context();
+	output_fmt = av_guess_format(NULL, "222.aac", NULL);
+	if (!output_fmt) {
+		av_log(NULL, AV_LOG_DEBUG, "Cloud not guess file format \n");
+		return;
+	}
+
+	ofmt_ctx->oformat = output_fmt;
+
+	out_stream = avformat_new_stream(ofmt_ctx, NULL);
+	if (!out_stream) {
+		av_log(NULL, AV_LOG_DEBUG, "Failed to create out stream!\n");
+		return;
+	}
+
+	if (fmt_ctx->nb_streams < 2) {
+		av_log(NULL, AV_LOG_ERROR, "the number of stream is too less!\n");
+		return;
+	}
+
+	if ((ret = avcodec_parameters_copy(out_stream->codecpar, in_codecpar)) < 0) {
+		av_log(NULL, AV_LOG_ERROR,"Failed to copy codec parameter, %d(%s)\n");
+	}
+
+	out_stream->codecpar->codec_tag = 0;
+
+	if ((ret = avio_open(&ofmt_ctx->pb, "222.aac", AVIO_FLAG_WRITE)) < 0) {
+		av_log(NULL, AV_LOG_DEBUG, "Could not open file! \n");
+	}
+
+	/*initialize packet*/
+	av_init_packet(&pkt);
+	pkt.data = NULL;
+	pkt.size = 0;
+
+	/*find best audio stream*/
+	audio_stream_index = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+	if (audio_stream_index < 0) {
+		av_log(NULL, AV_LOG_ERROR, "Failed to find Audio Stream!\n");
+	}
+
+	if (avformat_write_header(ofmt_ctx, NULL) < 0) {
+		av_log(NULL, AV_LOG_DEBUG, "Error occurred when opening output file");
+	}
+
+	/*read frames from media file*/
+	while (av_read_frame(fmt_ctx, &pkt) >= 0) {
+		if (pkt.stream_index == audio_stream_index) {
+			pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+			pkt.dts = pkt.pts;
+			pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+			pkt.pos = -1;
+			pkt.stream_index = 0;
+			av_interleaved_write_frame(ofmt_ctx, &pkt);
+			av_packet_unref(&pkt);
+		}
+	}
+
+	av_write_trailer(ofmt_ctx);
+
+	/*close input media file*/
+	avformat_close_input(&fmt_ctx);
+	if (dst_fd) {
+		fclose(dst_fd);
+	}
+
+	avio_close(ofmt_ctx->pb);
+	return;
+
+
 }
 
 // 使用FFmpeg从视频中抽取视频
@@ -558,10 +675,11 @@ int main(int argc, char* argv[]) {
 	//ffmpegVideoMeta(); // FFmpeg打印多媒体文件的Meta信息
 
 	/** 7.使用FFmpeg从视频中抽取音频 **/
-	//extractAudio();
+	//extractAudio_HE_ACC(); // (仅支持音频为HE-ACC格式的MP4)
+	//extractAudio();  // 不限格式，使用FFmpeg写入头
 
 	/** 8.使用FFmpeg从视频中抽取视频 **/
-	// extractVideo();
+	//extractVideo();
 
 	/** 至此基本的单元的操作内容基本完成，后续要学习的是更深一层的知识 */
 
