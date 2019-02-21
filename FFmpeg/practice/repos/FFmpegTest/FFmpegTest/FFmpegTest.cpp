43193cc7b7803c6bc42ac2133ad41c04bba41dca
灰色飘零 <1418757529@qq.com>
2019-02-21 19:05:03 +0800

    提交MP4转FLV的代码

--- FFmpeg/practice/repos/FFmpegTest/FFmpegTest/FFmpegTest.cpp
+++ FFmpeg/practice/repos/FFmpegTest/FFmpegTest/FFmpegTest.cpp
@@ -31,6 +31,7 @@ extern "C"{
 #include <stdio.h>
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
+#include "libavutil/timestamp.h"
 #include "libavutil/log.h"
 }
 
@@ -647,6 +648,149 @@ void extractVideo() {
 	}
 }
 
+
+// 使用FFmpeg将mp4转成flv
+void ffmpegMP42FLV() {
+
+	// 核心API
+
+	// avformat_alloc_output_context2() / avformat_free_context
+	// avformat_new_stream  
+	// avcodec_parameters_copy  
+	// avformat_write_header
+	// av_write_frame / av_interleaved_write_frame
+	// av_write_trailer
+
+	AVOutputFormat *ofmt = NULL;
+	AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
+	AVPacket pkt;
+	const char *in_filename, *out_filename;
+	int ret, i;
+	int stream_index = 0;
+	int *stream_mapping = NULL;
+	int stream_mapping_size = 0;
+
+	av_register_all();
+
+	if ((ret = avformat_open_input(&ifmt_ctx, "111.mp4", 0, 0)) < 0) {
+		goto end;
+	}
+
+	if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+		goto end;
+	}
+
+	av_dump_format(ifmt_ctx, 0, "111.mp4", 0);
+
+	avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, "111.flv");
+
+	if (!ofmt_ctx) {
+		fprintf(stderr, "Could not create output context\n");
+		ret = AVERROR_UNKNOWN;
+		goto end;
+	}
+
+	stream_mapping_size = ifmt_ctx->nb_streams;
+	stream_mapping = (int*) av_mallocz_array(stream_mapping_size, sizeof(*stream_mapping));
+	if (!stream_mapping) {
+		ret = AVERROR(ENOMEM);
+		goto end;
+	}
+
+	ofmt = ofmt_ctx->oformat;
+
+	for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+		AVStream *out_stream;
+		AVStream *in_stream = ifmt_ctx->streams[i];
+		AVCodecParameters *in_codecpar = in_stream->codecpar;
+
+		if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
+			in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO &&
+			in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
+			stream_mapping[i] = -1;
+			continue;
+		}
+
+		stream_mapping[i] = stream_index++;
+
+		out_stream = avformat_new_stream(ofmt_ctx, NULL);
+		if (!out_stream) {
+			fprintf(stderr, "Failed allocating output stream\n");
+			ret = AVERROR_UNKNOWN;
+			goto end;
+		}
+
+		ret = avcodec_parameters_copy(out_stream->codecpar, in_codecpar);
+		if (ret < 0) {
+			fprintf(stderr, "Failed to copy codec parameters\n");
+			goto end;
+		}
+		out_stream->codecpar->codec_tag = 0;
+	}
+	av_dump_format(ofmt_ctx, 0, "111.flv", 1);
+
+	if (!(ofmt->flags & AVFMT_NOFILE)) {
+		ret = avio_open(&ofmt_ctx->pb, "111.flv", AVIO_FLAG_WRITE);
+		if (ret < 0) {
+			goto end;
+		}
+	}
+
+	ret = avformat_write_header(ofmt_ctx, NULL);
+	if (ret < 0) {
+		fprintf(stderr, "Error occurred when opening output file\n");
+		goto end;
+	}
+
+	while (1) {
+		AVStream *in_stream, *out_stream;
+
+		ret = av_read_frame(ifmt_ctx, &pkt);
+		if (ret < 0)
+			break;
+
+		in_stream = ifmt_ctx->streams[pkt.stream_index];
+		if (pkt.stream_index >= stream_mapping_size ||
+			stream_mapping[pkt.stream_index] < 0) {
+			av_packet_unref(&pkt);
+			continue;
+		}
+
+		pkt.stream_index = stream_mapping[pkt.stream_index];
+		out_stream = ofmt_ctx->streams[pkt.stream_index];
+
+		/* copy packet */
+		pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base,(AVRounding) (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+		pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base,(AVRounding) (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+		pkt.pos = -1;
+
+		ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+		if (ret < 0) {
+			fprintf(stderr, "Error muxing packet\n");
+			break;
+		}
+		av_packet_unref(&pkt);
+	}
+
+	av_write_trailer(ofmt_ctx);
+end:
+
+	avformat_close_input(&ifmt_ctx);
+
+	/* close output */
+	if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+		avio_closep(&ofmt_ctx->pb);
+	avformat_free_context(ofmt_ctx);
+
+	av_freep(&stream_mapping);
+
+	if (ret < 0 && ret != AVERROR_EOF) {
+		fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
+		return;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	
 	/** 0.FFmpeg Hello World **/
@@ -681,9 +825,11 @@ int main(int argc, char* argv[]) {
 	/** 8.使用FFmpeg从视频中抽取视频 **/
 	//extractVideo();
 
+	ffmpegMP42FLV();
+
 	/** 至此基本的单元的操作内容基本完成，后续要学习的是更深一层的知识 */
 
-	/** 未来可能需要更进一步学习的是，这8个Demo涉及到的知识点，然后再往后继续学习 */
+	/** 未来可能需要更进一步学习的是，这9个Demo涉及到的知识点，然后再往后继续学习 */
 
 	return 0;
 }
